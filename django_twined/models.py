caab1adc298d65ac65ee0aa17d8c3e78a29221de
cortadocodes <cortado.codes@protonmail.com>
2020-12-09 12:23:37 +0000

    FIX: Add missing MyModel class

--- django_twined/models.py
+++ django_twined/models.py
@@ -13,3 +13,21 @@ class AbstractModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class MyModel(AbstractModel):
+    """
+    This is how you test abstract classes in your library without adding concrete models: add the concrete model
+     to your test app. You'll need to make the migrations for the test app:
+       python manage.py makemigrations tests
+
+    """
+
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        app_label = "tests"
+
+    def __str__(self):
+        # Ensures that the abstract class __str__ method is covered in testing
+        return super(MyModel, self).__str__() + ' ("{}")'.format(self.name)
