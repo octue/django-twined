dfdab1e221df56be0b90bb33a6a233eda2b9751f
cortadocodes <cortado.codes@protonmail.com>
2020-12-14 12:55:52 +0000

    REF: Make test case a Django test case

--- tests/test_my_consumer.py
+++ tests/test_my_consumer.py
@@ -1,18 +1,20 @@
 import pytest
 from channels.testing import WebsocketCommunicator
+from django.test import TestCase
 
 from tests.test_server.asgi import application
 
 
-@pytest.mark.asyncio
-async def test_my_consumer():
+class ConsumerTestCase(TestCase):
+    @pytest.mark.asyncio
+    async def test_my_consumer(self):
 
-    communicator = WebsocketCommunicator(application, "/my-consumer/")
-    connected, subprotocol = await communicator.connect()
-    assert connected
+        communicator = WebsocketCommunicator(application, "/my-consumer/")
+        connected, subprotocol = await communicator.connect()
+        assert connected
 
-    await communicator.send_to("ping-a-message")
-    pong = await communicator.receive_from()  # noqa: F841
-    assert pong == "ping-a-message"
+        await communicator.send_to("ping-a-message")
+        pong = await communicator.receive_from()  # noqa: F841
+        assert pong == "ping-a-message"
 
-    await communicator.disconnect()
+        await communicator.disconnect()
