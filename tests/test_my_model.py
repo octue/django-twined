a1b93cf1c53f8ecd9e87cecc4b37bcd255ee935a
Tom Clark <tom@octue.com>
2022-02-13 12:51:21 +0000

    TST: Remove example test case

--- tests/test_my_model.py
+++ tests/test_my_model.py
@@ -1,13 +1,13 @@
 # Disables for testing:
 # pylint: disable=missing-docstring
 
-from django.test import TestCase
-from django_twined.models import MyModel
+# from django.test import TestCase
+# from django_twined.models import MyModel
 
 
-class MyModelTestCase(TestCase):
-    """ "Normal" synchronous django tests to ensure your models / rest API / Whatever works correctly"""
+# class MyModelTestCase(TestCase):
+#     """ "Normal" synchronous django tests to ensure your models / rest API / Whatever works correctly"""
 
-    def test_something(self):
-        """Test that something happens"""
-        MyModel()
+#     def test_something(self):
+#         """Test that something happens"""
+#         MyModel()
