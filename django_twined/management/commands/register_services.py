fd1f607ca9b031de92a9caffd39ec4c6e460f41a
Tom Clark <tom@octue.com>
2022-02-13 12:50:48 +0000

    REF: Remove unused mixin

--- django_twined/management/commands/register_services.py
+++ django_twined/management/commands/register_services.py
@@ -4,14 +4,12 @@ from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from django_twined.models import RegisteredService
 
-from test.make import MakeMethodsMixin
-
 
 logger = logging.getLogger(__name__)
 User = get_user_model()
 
 
-class Command(MakeMethodsMixin, BaseCommand):
+class Command(BaseCommand):
     """Use `python manage.py help register_services` to display help for this command line administration tool"""
 
     help = "Registers the services available in settings into the database, enabling relation to services and asks."
