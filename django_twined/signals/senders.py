6d036b0aba9a003e73c79d8042ec82ff86798da7
Tom Clark <tom@octue.com>
2022-10-03 23:20:11 +0000

    FIX: Typo in signal name

--- django_twined/signals/senders.py
+++ django_twined/signals/senders.py
@@ -2,7 +2,7 @@ from django.dispatch import Signal
 
 
 delivery_acknowledgement_received = Signal()
-hearbeat_received = Signal()
+heartbeat_received = Signal()
 log_record_received = Signal()
 monitor_message_received = Signal()
 result_received = Signal()
