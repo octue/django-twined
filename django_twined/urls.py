ab5f6dcb0e5088ff30e5a478343a6e3dff945c5c
cortadocodes <cortado.codes@protonmail.com>
2023-06-29 13:39:57 +0100

    FIX: Make revision tag an optional URL field

--- django_twined/urls.py
+++ django_twined/urls.py
@@ -4,4 +4,5 @@ from django_twined.views import service_revision
 
 urlpatterns = [
     path(r"services/<namespace>/<name>/<revision_tag>", service_revision, name="service-revisions"),
+    path(r"services/<namespace>/<name>", service_revision, name="service-revisions"),
 ]
