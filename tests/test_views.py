666cc5beef01583ce892553d7ea33ab7b9306ecc
cortadocodes <cortado.codes@protonmail.com>
2023-06-29 13:40:17 +0100

    TST: Test getting latest service revision

--- tests/test_views.py
+++ tests/test_views.py
@@ -17,5 +17,21 @@ class TestServiceRevision(TestCase):
 
         self.assertEqual(
             response.json(),
-            {"name": "my-service", "namespace": "my-org", "revision_tag": "1.0.0", "success": True},
+            {"namespace": "my-org", "name": "my-service", "revision_tag": "1.0.0", "success": True},
+        )
+
+    def test_get_service_revision_without_revision_tag(self):
+        """Test that the latest service revision is returned when the revision tag isn't supplied."""
+        namespace = "my-org"
+        name = "my-service"
+
+        ServiceRevision.objects.create(namespace=namespace, name=name, tag="0.1.0")
+        ServiceRevision.objects.create(namespace=namespace, name=name, tag="1.0.0")
+        latest_service_revision = ServiceRevision.objects.create(namespace=namespace, name=name, tag="1.0.1")
+
+        response = self.client.get(reverse("service-revisions", kwargs={"namespace": namespace, "name": name}))
+
+        self.assertEqual(
+            response.json(),
+            {"namespace": namespace, "name": name, "revision_tag": latest_service_revision.tag, "success": True},
         )
