b6ec0c337e36b7590fb5d55547b205f8c9e29c7b
cortadocodes <cortado.codes@protonmail.com>
2023-07-04 16:43:29 +0100

    DOC: Add missing docstring details

--- django_twined/utils/versions.py
+++ django_twined/utils/versions.py
@@ -5,9 +5,9 @@ from django_twined.models import ServiceRevision
 def select_latest_service_revision_by_semantic_version(namespace, name):
     """Select the latest service revision by the semantic version used for its tag.
 
-    :param str namespace:
-    :param str name:
-    :return django_twined.models.service_revisions.ServiceRevision:
+    :param str namespace: the namespace of the service
+    :param str name: the name of the service
+    :return django_twined.models.service_revisions.ServiceRevision: the service revision whose tag is the latest semantic version
     """
     sorted_service_revisions = sorted(
         ServiceRevision.objects.filter(namespace=namespace, name=name),
