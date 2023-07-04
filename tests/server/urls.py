bcfd24e60285ad9b8ea12f7ac92ae7eed11f6f62
cortadocodes <cortado.codes@protonmail.com>
2023-07-04 15:38:03 +0100

    REF: Specify URL patterns in test server consistently

--- tests/server/urls.py
+++ tests/server/urls.py
@@ -1,11 +1,13 @@
+import django_gcp.urls
+import django_twined.urls
 from django.conf.urls import include, re_path
 from django.contrib import admin
-from django_gcp import urls as django_gcp_urls
 
 
 admin.autodiscover()
+
 urlpatterns = [
     re_path(r"^admin/", admin.site.urls),
-    re_path(r"^gcp/", include(django_gcp_urls)),
-    re_path(r"^integrations/octue/", include("django_twined.urls")),
+    re_path(r"^gcp/", include(django_gcp.urls)),
+    re_path(r"^integrations/octue/", include(django_twined.urls)),
 ]
