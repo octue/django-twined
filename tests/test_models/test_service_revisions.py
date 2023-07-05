55043853b46e6140b6505dc1cb9824e1a116952d
cortadocodes <cortado.codes@protonmail.com>
2023-07-05 17:20:59 +0100

    TST: Test only one revision of a service can be the default
    skipci

--- tests/test_models/test_service_revisions.py
+++ tests/test_models/test_service_revisions.py
@@ -64,27 +64,41 @@ class TestServiceRevisionIsLatestSemanticVersion(TestCase):
 
 class ServiceRevisionTestCase(TestCase):
     def test_topic(self):
-        """Ensure that default settings are read for creating default entries in the db"""
+        """Ensure that a service revision's topic is correct."""
         sr = ServiceRevision.objects.create(
-            project_name="gargantuan-gibbons", namespace="large-gibbons", name="gibbon-analyser", tag="latest"
+            project_name="gargantuan-gibbons",
+            namespace="large-gibbons",
+            name="gibbon-analyser",
+            tag="latest",
         )
 
         self.assertEqual(sr.topic, "octue.services.large-gibbons.gibbon-analyser.latest")
 
     def test_topic_with_blank_namespace_and_tag(self):
-        """Ensure that default settings are read for creating default entries in the db"""
+        """Ensure that a service revision's topic is correct if there is no namespace or tag."""
         with override_settings(TWINED_DEFAULT_NAMESPACE=""):
             with override_settings(TWINED_DEFAULT_TAG=""):
                 sr = ServiceRevision.objects.create(project_name="gargantuan-gibbons", name="gibbon-analyser")
-            self.assertEqual(sr.topic, "octue.services.gibbon-analyser")
+
+        self.assertEqual(sr.topic, "octue.services.gibbon-analyser")
 
     def test_create_with_defaults(self):
         """Ensure that default settings are read for creating default entries in the db"""
-
-        sr = ServiceRevision.objects.create(
-            name="gibbon-analyser",
-        )
-
+        sr = ServiceRevision.objects.create(name="gibbon-analyser")
         self.assertEqual(sr.namespace, "test-default-namespace")
         self.assertEqual(sr.project_name, "test-default-project-name")
         self.assertEqual(sr.tag, "test-default-tag")
+
+    def test_only_one_revision_of_a_service_can_be_default(self):
+        """Test that, when a new default revision is created for a service, the previous default revision has its
+        default status removed.
+        """
+        namespace = "large-gibbons"
+        name = "gibbon-analyser"
+
+        ServiceRevision.objects.create(namespace=namespace, name=name, tag="0.1.0", is_default=True)
+        self.assertTrue(ServiceRevision.objects.get(namespace=namespace, name=name, tag="0.1.0").is_default)
+
+        ServiceRevision.objects.create(namespace=namespace, name=name, tag="0.2.0", is_default=True)
+        self.assertTrue(ServiceRevision.objects.get(namespace=namespace, name=name, tag="0.2.0").is_default)
+        self.assertFalse(ServiceRevision.objects.get(namespace=namespace, name=name, tag="0.1.0").is_default)
